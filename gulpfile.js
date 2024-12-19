39d35e48bc756974a4e5a6c3d8888e6d25e05e75
HaHan <ha.han@fruitful.io>
2024-12-19 21:38:30 +0700

    (+) postcss

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,10 @@
 
 const build = require('@microsoft/sp-build-web');
 
+// Suppress warnings for CSS class names
 build.addSuppression(`Warning - [sass] The local CSS class 'ms-Grid' is not camelCase and will not be type-safe.`);
 
+// Override serve task to use 'serve-deprecated'
 var getTasks = build.rig.getTasks;
 build.rig.getTasks = function () {
   var result = getTasks.call(build.rig);
@@ -13,4 +15,16 @@ build.rig.getTasks = function () {
   return result;
 };
 
+// Force SPFx to use PostCSS 8
+build.configureWebpack.mergeConfig({
+  additionalConfiguration: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      postcss: require.resolve('postcss'), // Use the installed PostCSS 8 version
+    };
+    return config;
+  },
+});
+
+// Initialize build process
 build.initialize(require('gulp'));
--- src/webparts/helloWorld/components/HelloWorld.tsx
+++ src/webparts/helloWorld/components/HelloWorld.tsx
@@ -24,29 +24,32 @@ const HelloWorld = () => {
   );
 
   return (
-    <ReactFlow
-      nodes={nodes}
-      nodeTypes={nodeTypes}
-      onNodesChange={(e) => {
-        console.log("node", e);
-        onNodesChange(e);
-      }}
-      edges={edges}
-      edgeTypes={edgeTypes}
-      onEdgesChange={(e) => {
-        console.log("edge", e);
-        onEdgesChange(e);
-      }}
-      onConnect={(e) => {
-        console.log("connect", e);
-        onConnect(e);
-      }}
-      fitView
-    >
-      <Background />
-      <MiniMap />
-      <Controls />
-    </ReactFlow>
+    <>
+    <div>123123</div>
+      <ReactFlow
+        nodes={nodes}
+        nodeTypes={nodeTypes}
+        onNodesChange={(e) => {
+          console.log("node", e);
+          onNodesChange(e);
+        }}
+        edges={edges}
+        edgeTypes={edgeTypes}
+        onEdgesChange={(e) => {
+          console.log("edge", e);
+          onEdgesChange(e);
+        }}
+        onConnect={(e) => {
+          console.log("connect", e);
+          onConnect(e);
+        }}
+        fitView
+      >
+        <Background />
+        <MiniMap />
+        <Controls />
+      </ReactFlow>
+    </>
   );
 };
 
